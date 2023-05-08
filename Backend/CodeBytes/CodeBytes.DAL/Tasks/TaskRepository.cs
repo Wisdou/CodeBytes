fd2c4021795ea7da90a1c5b260d4d091063f9efb
Wisdou <51473681+Wisdou@users.noreply.github.com>
2023-05-08 17:13:10 +0300

    Init project

---
+++ Backend/CodeBytes/CodeBytes.DAL/Tasks/TaskRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBytes.DAL.Tasks
+{
+    class TaskRepository : ITaskRepository
+    {
+        public TaskEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(TaskEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
