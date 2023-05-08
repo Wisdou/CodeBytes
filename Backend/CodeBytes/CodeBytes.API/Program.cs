ee39c7ff96e7da1fc554bfc6aa6c96d9ba5d1f29
Wisdou <51473681+Wisdou@users.noreply.github.com>
2023-05-08 20:34:32 +0300

    Add Codewars reader

--- Backend/CodeBytes/CodeBytes.API/Program.cs
+++ Backend/CodeBytes/CodeBytes.API/Program.cs
@@ -6,13 +6,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeBytes.Reader.Codewars;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace CodeBytes.API
 {
     public class Program
     {
+        private const string PATH_TO_FILE = @"C:\Users\Daniel\OneDrive\Рабочий стол\Solutions.txt";
+
         public static void Main(string[] args)
         {
+            var list = CodeWarsReader.GetProblemsPerUser("andersk").Result;
+            File.WriteAllText(PATH_TO_FILE, JsonConvert.SerializeObject(list));
             CreateHostBuilder(args).Build().Run();
         }
 
