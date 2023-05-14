db0f13d0ae928562110437d9731261eaea4a7393
Wisdou <51473681+Wisdou@users.noreply.github.com>
2023-05-14 20:31:30 +0300

    Add domain

---
+++ Backend/CodeBytes/Domain/Interfaces/IProblemService.cs
@@ -0,0 +1,20 @@
+﻿using CodeBytes.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBytes.Domain.Interfaces
+{
+    public interface IProblemService
+    {
+        public void SaveProblem(Problem problemDTO);
+
+        public void SaveProblems(IEnumerable<Problem> problemDTOs);
+
+        public Problem GetProblem(int id);
+
+        public List<Problem> GetProblems();
+    }
+}
