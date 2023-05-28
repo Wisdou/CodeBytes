1ce5b1f4e007c274213db96683de7c5d42e0cd74
Wisdou <51473681+Wisdou@users.noreply.github.com>
2023-05-28 19:11:57 +0300

    Add migrations

---
+++ Backend/CodeBytes/CodeBytes.DAL/Migrations/20230528153457_create.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CodeBytes.DAL.Migrations
+{
+    public partial class create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Problems",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Problems", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProblemTags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Tag = table.Column<string>(type: "text", nullable: true),
+                    ProblemEntityID = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProblemTags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProblemTags_Problems_ProblemEntityID",
+                        column: x => x.ProblemEntityID,
+                        principalTable: "Problems",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProblemTags_ProblemEntityID",
+                table: "ProblemTags",
+                column: "ProblemEntityID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProblemTags");
+
+            migrationBuilder.DropTable(
+                name: "Problems");
+        }
+    }
+}
---
+++ Backend/CodeBytes/CodeBytes.DAL/Migrations/20230528153834_update1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeBytes.DAL.Migrations
+{
+    public partial class update1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProblemTags_Problems_ProblemEntityID",
+                table: "ProblemTags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProblemTags_ProblemEntityID",
+                table: "ProblemTags");
+
+            migrationBuilder.DropColumn(
+                name: "ProblemEntityID",
+                table: "ProblemTags");
+
+            migrationBuilder.CreateTable(
+                name: "ProblemEntityProblemTagEntity",
+                columns: table => new
+                {
+                    ProblemID = table.Column<int>(type: "integer", nullable: false),
+                    TagsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProblemEntityProblemTagEntity", x => new { x.ProblemID, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_ProblemEntityProblemTagEntity_Problems_ProblemID",
+                        column: x => x.ProblemID,
+                        principalTable: "Problems",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProblemEntityProblemTagEntity_ProblemTags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "ProblemTags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProblemEntityProblemTagEntity_TagsId",
+                table: "ProblemEntityProblemTagEntity",
+                column: "TagsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProblemEntityProblemTagEntity");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProblemEntityID",
+                table: "ProblemTags",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProblemTags_ProblemEntityID",
+                table: "ProblemTags",
+                column: "ProblemEntityID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProblemTags_Problems_ProblemEntityID",
+                table: "ProblemTags",
+                column: "ProblemEntityID",
+                principalTable: "Problems",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
