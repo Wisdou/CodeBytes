e7ead75bca3a24a7f4905747c2dd51883d83ca5f
Wisdou <51473681+Wisdou@users.noreply.github.com>
2023-06-13 02:55:38 +0300

    Add data for charts

--- Frontend/code-bytes/src/app/components/charts-islands/charts-islands.component.ts
+++ Frontend/code-bytes/src/app/components/charts-islands/charts-islands.component.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { TuiLabelModule } from '@taiga-ui/core';
+import { distinct, Observable, of } from 'rxjs';
+import { ProblemFilter, ProblemsDTO } from 'src/app/services/problem.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-charts-islands',
@@ -6,18 +16,36 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./charts-islands.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChartsIslandsComponent {
-  private readonly labels = ['Food', 'Cafe', 'Open Source', 'Taxi', 'other'];
-  readonly value = [13769, 12367, 10172, 3018, 2592];
-  readonly total = [...this.value].reduce((acc, x) => acc + x, 0);
+export class ChartsIslandsComponent implements OnInit {
+  @Input() dataSource: (initFunc?: () => void) => Observable<ProblemsDTO> =
+    () => of({ total: 0, problems: [] });
+
+  labels: string[] = [];
+  value: number[] = [];
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  get total(): number {
+    return [...this.value].reduce((acc, x) => acc + x, 0);
+  }
 
   index = NaN;
 
   get sum(): number {
-      return Number.isNaN(this.index) ? this.total : this.value[this.index];
+    return Number.isNaN(this.index) ? this.total : this.value[this.index];
   }
 
   get label(): string {
-      return Number.isNaN(this.index) ? 'Total' : this.labels[this.index];
+    return Number.isNaN(this.index) ? 'Total' : this.labels[this.index];
+  }
+
+  ngOnInit(): void {
+    this.dataSource().subscribe((problems) => {
+      const data = problems.problems;
+      const problemGroups = _.groupBy(data, (val) => val.difficulty);
+      this.labels = _.keys(problemGroups);
+      this.value = this.labels.map((val) => problemGroups[val].length);
+      this.cdRef.markForCheck();
+    });
   }
 }
