746d8787a1cebcbaed447187bc55e234748a75ed
Wisdou <51473681+Wisdou@users.noreply.github.com>
2023-07-12 01:38:43 +0300

    Switch to reactive forms in code-editor

--- Frontend/code-bytes/src/app/components/code-editor/code-editor.component.ts
+++ Frontend/code-bytes/src/app/components/code-editor/code-editor.component.ts
@@ -1,11 +1,17 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnChanges, Output, EventEmitter } from '@angular/core';
 import { first } from 'rxjs';
 import * as signalR from '@aspnet/signalr';
 import { SolutionService } from 'src/app/services/solution.service';
 import { HttpClient } from '@angular/common/http';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Problem } from '../../services/problem.service';
 
 declare var monaco: any;
+
+export interface SolutionCode{
+  code: string,
+  language: string,
+}
 
 @Component({
   selector: 'app-code-editor',
@@ -13,11 +19,9 @@ declare var monaco: any;
   styleUrls: ['./code-editor.component.scss'],
 })
 export class CodeEditorComponent implements OnInit {
-  editorOptions = {theme: 'vs-dark', language: 'javascript'};
-  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
-  chosenLanguage: string = "C#";
+  @Output() currentCode = new EventEmitter<SolutionCode>();
 
-  items: readonly string[] = [
+   Possible_Languages: readonly string[] = [
     "C#",
     "Python",
     "Go",
@@ -25,13 +29,28 @@ export class CodeEditorComponent implements OnInit {
     "C++"
   ];
 
-  problemForm: FormControl = new FormControl(this.items[0]);
+  editorOptions = {theme: 'vs-dark', language: 'javascript'};
   
-  constructor(public solutionService: SolutionService, private httpClient: HttpClient){}
+  problemForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){}
 
   ngOnInit(): void {
-    this.solutionService.startConnection();
-    this.solutionService.solutionListener('Wisdou' + Math.random() * 100);
-    this.httpClient.get('https://localhost:5001/api/solution').subscribe((res) => console.log(res));
+    this.problemForm = this.fb.group(
+      {
+        code: new FormControl('function x() {\nconsole.log("Hello world!");\n}'),
+        language: new FormControl(this.Possible_Languages[0]),
+      }
+    );
+    this.problemForm.valueChanges.subscribe(form => {
+      const userCode = {
+        language: form.language,
+        code: form.code,
+      };
+      this.currentCode.emit(userCode);
+    });
+    // this.solutionService.startConnection();
+    // this.solutionService.solutionListener('Wisdou' + Math.random() * 100);
+    // this.httpClient.get('https://localhost:5001/api/solution').subscribe((res) => console.log(res));
   }
 }
